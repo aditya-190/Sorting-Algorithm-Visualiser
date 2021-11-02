5a61fe39a5600114478ac6626416ffa9336fabd2
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2021-11-02 23:54:25 +0530

    Generate Array and Updating Array Count is now fully supportable.

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
+// Constants
 const CHANGE_SPEED = "Change Speed";
 const CHANGE_SIZE = "Change Size";
 const GENERATE_NEW_ARRAY = "Generate New Array";
 const GO = "Go";
 
+// Navigation Bar - Starts
 const tabs = $('.tabs');
 const activeItem = tabs.find('.active');
 const activeWidth = activeItem.innerWidth();
 
 changeHeading(activeItem.text() + " Sort");
-defaultActiveBarPosition();
-navigationBarClickListener();
 
 function changeHeading(heading) {
     $(".heading").text(heading);
 }
+
+defaultActiveBarPosition();
 
 function defaultActiveBarPosition() {
     $(".selector").css({
@@ -22,7 +24,9 @@ function defaultActiveBarPosition() {
     });
 }
 
-function navigationBarClickListener() {
+clickListeners();
+
+function clickListeners() {
     $(".tabs").on("click", "a", function (e) {
         e.preventDefault();
         const activeTab = $('.tabs a');
@@ -32,11 +36,11 @@ function navigationBarClickListener() {
             itemPos = $(this).parent().position();
             $("#change_speed").css("display", "block")
             $("#change_size").css("display", "none")
-        } else if($(this).text() === CHANGE_SIZE) {
+        } else if ($(this).text() === CHANGE_SIZE) {
             itemPos = $(this).parent().position();
             $("#change_size").css("display", "block")
             $("#change_speed").css("display", "none")
-        } else{
+        } else {
             itemPos = $(this).position();
             $("#change_speed").css("display", "none")
             $("#change_size").css("display", "none")
@@ -73,15 +77,67 @@ function navigationBarClickListener() {
             }
         }
     });
+
+    $(document).on('input', '#speed_slider', function () {
+        updateSpeed($(this).val());
+    });
+
+    $(document).on('input', '#size_slider', function () {
+        updateSize($(this).val());
+    });
+}
+
+// Navigation Bar - Ends
+
+
+// Generate Array - Starts
+const sizeSlider = $("#size_slider");
+let totalBars = sizeSlider.val()
+let singleBarSize = [];
+let divsArray = [];
+let spaceBetweenBars = 0.1;
+
+const content = $("#mainContent");
+content.css("flex-direction", "row");
+
+generateArray()
+
+function generateArray() {
+    content.empty();
+
+    for (let i = 0; i < totalBars; i++) {
+        singleBarSize[i] = Math.floor(Math.random() * 0.5 * (sizeSlider.attr('max') - sizeSlider.attr('min'))) + 10;
+        divsArray[i] = $('<div></div>');
+        content.append(divsArray[i]);
+        divsArray[i].css({
+                "margin": "0% " + spaceBetweenBars + "%",
+                "background": "#05abe0",
+                "background": "-moz-linear-gradient(45deg, #05abe0 0%, #8200f4 100%)",
+                "background": "-webkit-linear-gradient(45deg, #05abe0 0%, #8200f4 100%)",
+                "background": "linear-gradient(45deg, #05abe0 0%, #8200f4 100%)",
+                "filter": "gradient(startColorstr='#05abe0', endColorstr='#8200f4', GradientType=1)",
+                "width": (100 / totalBars - (2 * spaceBetweenBars)) + "%",
+                "height": singleBarSize[i] + "%"
+            }
+        );
+    }
+}
+
+// Generate Array - Ends
+
+
+function updateSize(changedSize) {
+    totalBars = changedSize
+    generateArray()
+}
+
+function updateSpeed(changedSpeed) {
 }
 
 function changeSpeed() {
 }
 
 function changeSize() {
-}
-
-function generateArray() {
 }
 
 function startAlgorithmVisualisation() {
