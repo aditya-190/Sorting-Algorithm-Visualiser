0be396846d6ae31bbb12022adc8637648bf5f27d
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2021-11-03 15:25:37 +0530

    Insertion Sort Logic is Added.

--- scripts/insertion_sort.js
+++ scripts/insertion_sort.js
@@ -1,3 +1,10 @@
 function insertionSort() {
-
+    for (let i = 1; i < totalBars; i++) {
+        let currentSelected = singleBarSize[i], j = i - 1;
+        while (j >= 0 && singleBarSize[j] > currentSelected) {
+            singleBarSize[j + 1] = singleBarSize[j];
+            j = j - 1;
+        }
+        singleBarSize[j + 1] = currentSelected;
+    }
 }
