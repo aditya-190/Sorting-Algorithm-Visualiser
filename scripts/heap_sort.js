8eb86fbf8cde093abde84b1d3e9805cfb7cd7200
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2021-11-03 22:33:25 +0530

    Heap Sort Logic is Added.

--- scripts/heap_sort.js
+++ scripts/heap_sort.js
@@ -1,3 +1,26 @@
-function heapSort() {
+function helperFunction(size, currentLargest) {
+    let largest = currentLargest, left = 2 * currentLargest + 1, right = 2 * currentLargest + 2;
 
+    if (left < size && singleBarSize[left] > singleBarSize[largest]) largest = left;
+    if (right < size && singleBarSize[right] > singleBarSize[largest]) largest = right;
+
+    if (largest !== currentLargest) {
+        let temporary = singleBarSize[currentLargest];
+        singleBarSize[currentLargest] = singleBarSize[largest];
+        singleBarSize[largest] = temporary
+        helperFunction(size, largest);
+    }
+}
+
+function heapSort() {
+    let size = totalBars;
+    for (let i = Math.floor(size / 2) - 1; i >= 0; i--)
+        helperFunction(size, i);
+
+    for (let i = size - 1; i > 0; i--) {
+        let temporary = singleBarSize[0];
+        singleBarSize[0] = singleBarSize[i];
+        singleBarSize[i] = temporary;
+        helperFunction(i, 0);
+    }
 }
