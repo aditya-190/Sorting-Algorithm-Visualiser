1821fb0c9517191c7577d5dd3648a1d633d6061d
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2021-11-03 15:30:47 +0530

    Bubble Sort Logic is Added.

--- scripts/bubble_sort.js
+++ scripts/bubble_sort.js
@@ -1,3 +1,11 @@
 function bubbleSort() {
-
+    for (let i = 0; i < totalBars; i++) {
+        for (let j = 0; j < totalBars - i; j++) {
+            if (singleBarSize[j] > singleBarSize[j + 1]) {
+                let temporary = singleBarSize[j];
+                singleBarSize[j] = singleBarSize[j + 1];
+                singleBarSize[j + 1] = temporary;
+            }
+        }
+    }
 }
