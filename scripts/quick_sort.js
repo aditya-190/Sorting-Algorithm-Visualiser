3aeaafedcb1029577bef984e5c12d5da56f7240a
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2021-11-07 12:35:13 +0530

    1. Log Errors Resolved in Main Javascript File. 2. Bubble Sort More Visual Added for Clarification. 3. Quick Sort Visualisation [Pending]. 4. Insertion Sort Visualisation [Pending].

--- scripts/quick_sort.js
+++ scripts/quick_sort.js
@@ -1,26 +1,53 @@
 function quickSort(start, end) {
     if (start < end) {
-        let pi = divide(start, end);
-        quickSort(start, pi - 1);
-        quickSort(pi + 1, end);
+        let pivot = divide(start, end);
+        quickSort(start, pivot - 1);
+        quickSort(pivot + 1, end);
     }
+
+    enableButtons();
 }
 
-function divide(low, high) {
-    let pivot = singleBarSize[high];
-    let i = (low - 1);
+function divide(start, end) {
+    let pivot = singleBarSize[start];
+    let i = start + 1;
 
-    for (let j = low; j <= high - 1; j++) {
+    barsUpdater(divsArray[start], singleBarSize[start], "yellow");
+
+    for (let j = start + 1; j <= end - 1; j++) {
+        barsUpdater(divsArray[j], singleBarSize[j], "yellow");
+
         if (singleBarSize[j] < pivot) {
-            i++;
+            barsUpdater(divsArray[i], singleBarSize[i], "red");
+            barsUpdater(divsArray[j], singleBarSize[j], "red");
 
             let temporary = singleBarSize[i];
             singleBarSize[i] = singleBarSize[j]
             singleBarSize[j] = temporary;
+
+            barsUpdater(divsArray[i], singleBarSize[i], "red");
+            barsUpdater(divsArray[j], singleBarSize[j], "red");
+            barsUpdater(divsArray[i], singleBarSize[i], "blue");
+            barsUpdater(divsArray[j], singleBarSize[j], "blue");
+
+            i++;
+        } else {
+            barsUpdater(divsArray[j], singleBarSize[j], "blue");
         }
     }
-    let temporary = singleBarSize[i + 1];
-    singleBarSize[i + 1] = singleBarSize[high]
-    singleBarSize[high] = temporary;
-    return (i + 1);
+
+    barsUpdater(divsArray[start], singleBarSize[start], "red");
+    barsUpdater(divsArray[i - 1], singleBarSize[i - 1], "red");
+
+    let temporary = singleBarSize[start];
+    singleBarSize[start] = singleBarSize[i - 1]
+    singleBarSize[i - 1] = temporary;
+
+    barsUpdater(divsArray[start], singleBarSize[start], "red");
+    barsUpdater(divsArray[i - 1], singleBarSize[i - 1], "red");
+
+    for (let temporary = start; temporary <= i; temporary++)
+        barsUpdater(divsArray[temporary], singleBarSize[temporary], "green");
+
+    return (i - 1);
 }
