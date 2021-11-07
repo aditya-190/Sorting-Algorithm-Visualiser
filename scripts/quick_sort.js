4d60b8a5f849db97f7ff494279b7fa026de34adf
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2021-11-07 13:03:51 +0530

    Quick Sort Code For Visualisation Completed.

--- scripts/quick_sort.js
+++ scripts/quick_sort.js
@@ -8,46 +8,23 @@ function quickSort(start, end) {
     enableButtons();
 }
 
-function divide(start, end) {
-    let pivot = singleBarSize[start];
-    let i = start + 1;
+function divide(lowerBound, upperBound) {
+    let pivot = singleBarSize[lowerBound], start = lowerBound, end = upperBound;
 
-    barsUpdater(divsArray[start], singleBarSize[start], "yellow");
+    while (start < end) {
+        while (singleBarSize[start] <= pivot) start++;
+        while (singleBarSize[end] > pivot) end--;
 
-    for (let j = start + 1; j <= end - 1; j++) {
-        barsUpdater(divsArray[j], singleBarSize[j], "yellow");
-
-        if (singleBarSize[j] < pivot) {
-            barsUpdater(divsArray[i], singleBarSize[i], "red");
-            barsUpdater(divsArray[j], singleBarSize[j], "red");
-
-            let temporary = singleBarSize[i];
-            singleBarSize[i] = singleBarSize[j]
-            singleBarSize[j] = temporary;
-
-            barsUpdater(divsArray[i], singleBarSize[i], "red");
-            barsUpdater(divsArray[j], singleBarSize[j], "red");
-            barsUpdater(divsArray[i], singleBarSize[i], "blue");
-            barsUpdater(divsArray[j], singleBarSize[j], "blue");
-
-            i++;
-        } else {
-            barsUpdater(divsArray[j], singleBarSize[j], "blue");
+        if (start < end) {
+            let temporary = singleBarSize[start];
+            singleBarSize[start] = singleBarSize[end]
+            singleBarSize[end] = temporary;
         }
     }
 
-    barsUpdater(divsArray[start], singleBarSize[start], "red");
-    barsUpdater(divsArray[i - 1], singleBarSize[i - 1], "red");
+    let temporary = singleBarSize[lowerBound];
+    singleBarSize[lowerBound] = singleBarSize[end]
+    singleBarSize[end] = temporary;
 
-    let temporary = singleBarSize[start];
-    singleBarSize[start] = singleBarSize[i - 1]
-    singleBarSize[i - 1] = temporary;
-
-    barsUpdater(divsArray[start], singleBarSize[start], "red");
-    barsUpdater(divsArray[i - 1], singleBarSize[i - 1], "red");
-
-    for (let temporary = start; temporary <= i; temporary++)
-        barsUpdater(divsArray[temporary], singleBarSize[temporary], "green");
-
-    return (i - 1);
+    return end;
 }
