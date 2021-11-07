6f88a378806165750d23081f0328f3d7e4838e42
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2021-11-07 12:55:27 +0530

    Insertion Sorting Visualisation Done.

--- scripts/insertion_sort.js
+++ scripts/insertion_sort.js
@@ -3,19 +3,31 @@ function insertionSort() {
         let currentSelected = singleBarSize[i], j = i - 1;
         barsUpdater(divsArray[i], singleBarSize[i], "yellow")
 
-        while (j >= 0 && singleBarSize[j] > currentSelected) {
+        while (j >= 0) {
             barsUpdater(divsArray[j], singleBarSize[j], "yellow")
-            barsUpdater(divsArray[j], singleBarSize[j], "red")
-            barsUpdater(divsArray[j + 1], singleBarSize[j + 1], "red")
 
-            singleBarSize[j + 1] = singleBarSize[j];
+            if (singleBarSize[j] > currentSelected) {
+                barsUpdater(divsArray[j], singleBarSize[j], "red")
+                barsUpdater(divsArray[j + 1], singleBarSize[j + 1], "red")
 
-            barsUpdater(divsArray[j], singleBarSize[j], "red")
-            barsUpdater(divsArray[j + 1], singleBarSize[j + 1], "red")
-            j = j - 1;
+                singleBarSize[j + 1] = singleBarSize[j];
+
+                barsUpdater(divsArray[j], singleBarSize[j], "red")
+                barsUpdater(divsArray[j + 1], singleBarSize[j + 1], "red")
+                barsUpdater(divsArray[j], singleBarSize[j], "blue")
+                barsUpdater(divsArray[j + 1], singleBarSize[j + 1], "blue")
+                j--;
+            } else {
+                barsUpdater(divsArray[j], singleBarSize[j], "blue")
+                break;
+            }
         }
         singleBarSize[j + 1] = currentSelected;
+
+        for (let temporary = 0; temporary < i; temporary++)
+            barsUpdater(divsArray[temporary], singleBarSize[temporary], "green")
     }
+    barsUpdater(divsArray[totalBars - 1], singleBarSize[totalBars - 1], "green")
 
     enableButtons();
 }
